915c926c82829e9d8a5f7d685e47dc4b6572c2bc
Ante <anteukic@hotmail.com>
2021-11-18 16:03:50 +0100

    Prvi commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Ante Ukića, 0165079508");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Ante Ukića");
+>>>>>>> 72cc2ee (Prvi commit)
         }
     }
 }
